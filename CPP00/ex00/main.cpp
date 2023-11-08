6bd89a7d5d6fd73edec06e8bcee1dc64c95dbc9d
705mmes <muniosamuel@gmail.com>
2023-11-08 16:30:29 +0100

    00 done

--- CPP00/ex00/main.cpp
+++ CPP00/ex00/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 15:11:48 by sammeuss          #+#    #+#             */
-/*   Updated: 2023/11/08 15:33:14 by sammeuss         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:29:30 by sammeuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 int	main(int ac, char **av)
 {
-	(void)av;
-	if (ac != 2)
+	int	i = -1;
+	int	u = 0;
+
+	if (ac < 2)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	while (av[++u])
 	{
-		std::cout << "Wrong amount of args" << std::endl;
-		return (1);
+		while (av[u][++i])
+			std::cout << (char) toupper(av[u][i]);
+		i = -1;
 	}
+	std::cout << std::endl;
 	return (0);
 }
