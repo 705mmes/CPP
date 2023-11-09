efe0471f3e69e18eae3acd8635ec207a1d57a171
705mmes <muniosamuel@gmail.com>
2023-11-09 11:44:04 +0100

    phonebook started

---
+++ CPP00/ex01/Phonebook.class.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.class.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 16:57:30 by sammeuss          #+#    #+#             */
+/*   Updated: 2023/11/08 17:03:42 by sammeuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.class.hpp"
+
+Phonebook::Phonebook()
+{
+	std::cout << "Phonebook constructor called" << std::endl;
+	return ;
+}
+
+Phonebook::~Phonebook()
+{
+	std::cout << "Phonebook destructor called" << std::endl;
+	return ;
+}
