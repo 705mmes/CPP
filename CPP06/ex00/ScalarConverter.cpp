7aa505b1a41972c16d74b4ad8d402c336f7f2256
Samuel Munio <smunio@k2r2p10.42mulhouse.fr>
2024-01-31 14:59:44 +0100

    fckdup

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smunio <smunio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:57:46 by sammeuss          #+#    #+#             */
-/*   Updated: 2024/01/30 13:26:18 by sammeuss         ###   ########.fr       */
+/*   Updated: 2024/01/31 14:59:29 by smunio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,115 @@ Scalar::~Scalar()
 
 void Scalar::convert(std::string s)
 {
-	/* return function that prints result */
+	if (is_char(s))
+		to_char(s);
+	else if(is_int(s))
+		to_int(s);
+	else if(is_float(s))
+		to_float(s);
+	else if(is_double(s))
+		to_double(s);
+	else
+		std::cerr << "Can't find type" << std::endl;
+	return ;
+}
+
+bool	is_double(std::string s)
+{
+	int d = 0;
+	int i = 0;
+
+	while(s[i])
+	{
+		if (s[i] != '.' && !(s[i] >= 48 && s[i] <= 57))
+			return (false);
+		if (s[i] == '.')
+			d++;
+		i++;
+	}
+	if (d == 1)
+		return (true);
+	else
+		return (false);
+}
+
+void	to_double(std::string s)
+{
+	double tmp = std::atof(s.c_str());
+
+	std::cout << "char: " << static_cast<char>(tmp) << std::endl;
+	std::cout << "int: " << static_cast<int>(tmp) << std::endl;
+	std::cout << "float: " << static_cast<float>(tmp) << std::fixed << std::endl;
+	std::cout << "double: " << tmp << std::fixed << std::endl;
+}
+
+bool	is_float(std::string s)
+{
+	int d = 0;
+	unsigned long i = 0;
+
+	while(i < s.length() - 1)
+	{
+		if (s[i] != '.' && !(s[i] >= 48 && s[i] <= 57))
+			return (false);
+		if (s[i] == '.')
+			d++;
+		i++;
+	}
+	if (d == 1 && s[i] == 'f')
+		return (true);
+	else
+		return (false);
+}
+
+void	to_float(std::string s)
+{
+	float	tmp = std::atof(s.c_str());
+
+	std::cout << "char: " << static_cast<char>(tmp) << std::endl;
+	std::cout << "int: " << static_cast<int>(tmp) << std::endl;
+	std::cout << "float: " << tmp << std::fixed <<  std::endl;
+	std::cout << "double: " << static_cast<double>(tmp) << std::fixed << std::endl;
+}
+
+bool	is_int(std::string s)
+{
+	for (int i = 0; s[i]; i++)
+	{
+		if(!(s[i] >= 48 && s[i] <= 57))
+			return (false);
+	}
+	if (std::atoi(s.c_str()) > 2147483647 || std::atoi(s.c_str()) < -2147483648)
+		return (false);
+	return (true);
+}
+
+void	to_int(std::string s)
+{
+	int tmp = std::atoi(s.c_str());
+
+	std::cout << "char: " << static_cast<char>(tmp) << std::endl;
+	std::cout << "int: " << tmp << std::endl;
+	std::cout << "float: " << static_cast<float>(tmp) << std::fixed << std::endl;
+	std::cout << "double: " << static_cast<double>(tmp) << std::fixed << std::endl;
+}
+
+bool	is_char(std::string s)
+{
+	int i = 0;
+	while(s[i])
+		i++;
+	if (i == 1 && s[i - 1] >= 33 && s[i - 1] <= 126)
+		return (true);
+	else
+		return (false);
+}
+
+void	to_char(std::string s)
+{
+	std::cout << "char: " << s[0] << std::endl;
+	std::cout << "int: " << std::atoi(s.c_str()) << std::endl;
+	std::cout << "float: " << static_cast<float>(atoi(s.c_str())) << std::fixed << std::endl;
+	std::cout << "double: " << static_cast<double>(atoi(s.c_str())) << std::fixed << std::endl;
 	return ;
 }
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -3,22 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smunio <smunio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:57:00 by sammeuss          #+#    #+#             */
-/*   Updated: 2024/01/30 13:05:16 by sammeuss         ###   ########.fr       */
+/*   Updated: 2024/01/31 14:12:33 by smunio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 # include <iostream>
+# include <string>
+# include <cstdlib>
+# include <limits>
 
 class Scalar
 {
-	Scalar();
-	~Scalar();
-	void static convert(std::string);
+	public:
+		void static convert(std::string);
+		~Scalar();
+	private:
+		Scalar();
 };
+
+bool	is_char(std::string s);
+bool	is_int(std::string s);
+bool	is_float(std::string s);
+bool	is_double(std::string s);
+
+void	to_char(std::string s);
+void	to_int(std::string s);
+void	to_float(std::string s);
+void	to_double(std::string s);
 
 #endif
