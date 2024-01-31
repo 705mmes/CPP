0b2b23fd0af54a25620a439d5888d5c4e2a65655
705mmes <muniosamuel@gmail.com>
2024-01-31 10:12:55 +0100

    ok

---
+++ CPP06/ex00/ScalarConverter.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 12:57:46 by sammeuss          #+#    #+#             */
+/*   Updated: 2024/01/30 13:26:18 by sammeuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+Scalar::Scalar()
+{
+	std::cout << "Default Scalar constructor called" << std::endl;
+	return ;
+}
+
+Scalar::~Scalar()
+{
+	std::cout << "Default Scalar destructor called" << std::endl;
+	return ;
+}
+
+void Scalar::convert(std::string s)
+{
+	/* return function that prints result */
+	return ;
+}
---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 12:57:00 by sammeuss          #+#    #+#             */
+/*   Updated: 2024/01/30 13:05:16 by sammeuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+# include <iostream>
+
+class Scalar
+{
+	Scalar();
+	~Scalar();
+	void static convert(std::string);
+};
+
+#endif
